c993e0cd33f60df9b8000c74a10317d24741fdeb
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-19 20:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	s=s.trimStart();
+	const firstSpaceIndex=s.indexOf(' ');
+
+	if(firstSpaceIndex===-1){
+		return s;
+	}
+	return s.substring(0,firstSpaceIndex);
 }
 
 // Do not change the code below
